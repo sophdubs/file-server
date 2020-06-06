16f166e5d0e398e2e29030e31253f8514d832735
Sophie <sophiemargueritedubois@gmail.com>
2020-06-06 01:39:35 +0000

    Client able to connect to server and provide file name. Server can successfully read file and sen data to client. Client able to write data to file with same filename but in a write folder

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const fs = require('fs');
 
 const fileName = process.argv.slice(2)[0];
 
@@ -11,6 +12,16 @@ const conn = net.createConnection({
 
 conn.on('connect', () => {
   conn.write(fileName);
+});
+
+conn.on('data', data => {
+  fs.writeFile(`./write/${fileName}`, data, (err) => {
+    if (err) {
+      console.log('An error occured while writing the file');
+    } else {
+      conn.end();
+    }
+  })
 });
 
 conn.setEncoding('utf8');
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const net = require('net');
+const fs = require('fs');
 const server = net.createServer();
 
 server.on('connection', (client) => {
   console.log('A new client connected');
   client.on('data', data => {
-    console.log(data.toString());
-    client.write('fetcthing data...');
+    fs.readFile(data.toString(), 'utf8', (err, file) => {
+      if (err) {
+        console.log(err);
+      } else {
+        client.write(file);
+      }
+    })
   });
 });
-
 
 server.listen(3000);
 
